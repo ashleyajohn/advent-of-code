1b3dc8d205a62dd997d93f3e93354edd5c353c6c
Ashley John <31779750+ashleyajohn@users.noreply.github.com>
2020-12-05 11:51:35 -0500

    add dec_2

---
+++ code/dec_2.py
@@ -0,0 +1,31 @@
+"""Day 2 exercise."""
+from typing import List, Tuple
+
+def parse_input_passwords(passwords: list) -> List[Tuple]:
+    """Parse password input.
+    
+    Parse password rule from sample password. 
+    Example: input of 2-3 r: jrfrvrk is returned
+    as ('2-3 r', 'jrfrvrk')
+    """
+    parsed_passwords = []
+    for item in passwords:
+        rule, password = item.split(':')
+        parsed_passwords.append((rule, password))
+    return parsed_passwords
+
+def check_valid_password_count(rule: str, password: str) -> bool:
+    """Check if a password is valid according to its rule based on char count."""
+    count_range, letter = rule.split(' ')
+    min_count = int(count_range.split('-')[0])
+    max_count = int(count_range.split('-')[1])
+    return min_count <= password.count(letter) <= max_count
+
+def check_valid_password_position(rule: str, password: str) -> bool:
+    """Check if a password is valid according to its rule based on char position."""
+    count_range, letter = rule.split(' ')
+    pos_a = int(count_range.split('-')[0])
+    pos_b = int(count_range.split('-')[1])
+    if sum((password[pos_a] == letter, password[pos_b] == letter)) == 1:
+        return True
+    return False
